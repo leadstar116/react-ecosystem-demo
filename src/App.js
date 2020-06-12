5bc8e2b35af77a62c6acc477aba3f732d1de3673
leadstar116 <supremedev116@gmail.com>
2020-06-12 22:04:49 +1000

    add webpack

--- src/App.js
+++ src/App.js
@@ -2,9 +2,11 @@ import React from 'react'
 import './App.css'
 
 const App = () => {
-    <div className="App">
-        <h2>Hello World!</h2>
-    </div>
+    return (
+        <div className="App">
+            <h2>Hello World!</h2>
+        </div>
+    )
 }
 
 export default App
