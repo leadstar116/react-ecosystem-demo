86473ce1f7c7f9d9edcc64bf4f646c5deaf86a10
leadstar116 <supremedev116@gmail.com>
2020-06-19 08:44:29 +1000

    add redux thunk

--- src/store.js
+++ src/store.js
@@ -1,11 +1,14 @@
-import { createStore, combineReducers } from 'redux'
-import { todos } from './todos/reducers'
+import { createStore, combineReducers, applyMiddleware } from 'redux'
+import { todos, isLoading } from './todos/reducers'
 import { persistReducer } from 'redux-persist'
+import thunk from 'redux-thunk'
+import { composeWithDevTools } from 'redux-devtools-extension'
 import storage from 'redux-persist/lib/storage'
 import autoMergeLevel2 from 'redux-persist/lib/stateReconciler/autoMergeLevel2'
 
 const reducers = {
     todos,
+    isLoading,
 }
 
 const persistConfig = {
@@ -20,6 +23,7 @@ const persistedReducer = persistReducer(persistConfig, rootReducer)
 export const configureStore = () =>
     createStore(
         persistedReducer,
-        window.__REDUX_DEVTOOLS_EXTENSION__ &&
-        window.__REDUX_DEVTOOLS_EXTENSION__()
+        composeWithDevTools(
+            applyMiddleware(thunk)
+        )
     )
--- src/todos/TodoList.js
+++ src/todos/TodoList.js
@@ -1,29 +1,41 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { connect } from 'react-redux'
 import TodoListItem from './TodoListItem'
 import NewTodoForm from './NewTodoForm'
+import { loadTodos } from './thunks'
 import { removeTodo, markAsCompleted } from './actions'
 import './TodoList.css'
+import { isLoading } from './reducers';
 
-const TodoList = ({todos = [], onRemovePressed, onMarkAsCompletedPressed}) => (
-    <div className="list-wrapper">
-        <NewTodoForm />
-        {
-            todos.map(todo =>
-                <TodoListItem
-                    todo={todo}
-                    onRemovePressed={onRemovePressed}
-                    onMarkAsCompletedPressed={onMarkAsCompletedPressed}
-                />)
-        }
-    </div>
-)
+const TodoList = ({todos = [], isLoading, onRemovePressed, onMarkAsCompletedPressed, startLoadingTodos}) => {
+    useEffect(() => {
+        startLoadingTodos()
+    }, [])
+
+    const loadingMessage = <div>Loading todos...</div>
+    const content = (
+        <div className="list-wrapper">
+            <NewTodoForm />
+            {
+                todos.map(todo =>
+                    <TodoListItem
+                        todo={todo}
+                        onRemovePressed={onRemovePressed}
+                        onMarkAsCompletedPressed={onMarkAsCompletedPressed}
+                    />)
+            }
+        </div>
+    )
+    return isLoading ? loadingMessage : content
+}
 
 const mapStateToProps = (state) => ({
+    isLoading: state.isLoading,
     todos: state.todos,
 })
 
 const mapDispatchToProps = (dispatch) => ({
+    startLoadingTodos: () => dispatch(loadTodos()),
     onRemovePressed: (text) => dispatch(removeTodo(text)),
     onMarkAsCompletedPressed: (text) => dispatch(markAsCompleted(text)),
 })
